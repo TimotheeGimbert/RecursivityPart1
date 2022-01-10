d8fd238906e04b2627c338f7f9ca7d185544aa88
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-10 20:53:06 +0100

    recursive power without dealing with negative exp

--- power.js
+++ power.js
@@ -1,6 +1,7 @@
 const powerIt = (n, p) => {
   if (n === 0) return 0; // zero to the power of any number is equal to 0
   if (p === 0) return 1; // any number to the power of zero is equal to 1
+
   let result = n;
   exp = Math.abs(p);
   while (exp > 1) {
@@ -13,8 +14,5 @@ const powerIt = (n, p) => {
 }
 
 const powerRec = (n, p) => {
-  if (p === 1) return n; // BASE CASE
-  else if (p >= 1) return n + computePowerRec(n, p - 1); // RECURSIVE CASE
-  else if (p === 0) return 1;
-  else return 0;
+  return p == 0 ? 1 : n * powerRec(n, --p); // BASE CASE + RECURSIVE CASE
 }
