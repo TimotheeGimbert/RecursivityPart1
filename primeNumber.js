b3ab45da1b8dcb7089f87e57afc22f19f1e627d4
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-11 03:42:30 +0100

    adds prime recursive

--- primeNumber.js
+++ primeNumber.js
@@ -7,6 +7,10 @@ const isPrimeNumber = (n) => { // return true if n is a prime number
   return bool;
 }
 
-const nullModulo = (a, b) => {
-  return a % b === 0 ? true : false;
+function isPrimeRec(num, div = 3){
+  if(num === 2) return true;
+  if(num < 2 || num % 2 === 0)  return false;
+  if(div * div > num) return true;
+  if(num % div === 0) return false;
+  return isPrimeRec(num, div + 2);
 }
