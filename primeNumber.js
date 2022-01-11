4acd15336666bdef25db346488ef16a917659f48
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-11 03:33:36 +0100

    prime number iterativ

--- primeNumber.js
+++ primeNumber.js
@@ -1,3 +1,12 @@
 const isPrimeNumber = (n) => { // return true if n is a prime number
+  let bool = true;
+  if (n === 0) bool = false;
+  for (let i = n-1; i > 1; i--) {
+    if (n % i === 0) bool = false;
+  }
+  return bool;
+}
 
+const nullModulo = (a, b) => {
+  return a % b === 0 ? true : false;
 }
